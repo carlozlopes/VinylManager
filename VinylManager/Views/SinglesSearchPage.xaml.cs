40cb264d7e66eb2d4497c57de5c924ffd3109d5b
clopez <carloz.lopes@gmail.com>
2015-09-08 17:08:16 -0500

    Modify SinglesSearchPage interface and click event

--- VinylManager/Views/SinglesSearchPage.xaml.cs
+++ VinylManager/Views/SinglesSearchPage.xaml.cs
@@ -66,7 +66,7 @@ namespace VinylManager.Views
             }
         }
 
-        private void ArtistesListView_ItemClick(object sender, ItemClickEventArgs e)
+        private void ArtistesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             selectedArtiste = (ArtisteViewModel)ArtistesListView.SelectedItem;
         }
