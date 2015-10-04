90877d2dfc3f2e136ca80612ffbba399ec94b492
clopez <carloz.lopes@gmail.com>
2015-10-04 16:37:45 -0500

    First working version of EtiquettesPage

--- VinylManager/Views/EtiquettesPage.xaml.cs
+++ VinylManager/Views/EtiquettesPage.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using VinylManager.ViewModel;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using iTextSharp;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Diagnostics;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -13,6 +17,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.System;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,7 +32,10 @@ namespace VinylManager.Views
         SinglesViewModel SinglesViewModel = new SinglesViewModel();
         SinglesSearchPageViewModel singlesSearchPageViewModel = new SinglesSearchPageViewModel();
         ArtisteViewModel selectedArtiste;
-        List<SingleJoinDataViewModel> SinglesToEtiquettes = new List<SingleJoinDataViewModel>(); 
+        List<SingleJoinDataViewModel> SinglesToEtiquettes = new List<SingleJoinDataViewModel>();
+        string path = @"Etiquettes.pdf";
+        // StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+        StorageFolder storageFolder = ApplicationData.Current.TemporaryFolder;
 
         public EtiquettesPage()
         {
@@ -124,6 +133,113 @@ namespace VinylManager.Views
                 SinglesEtiquettesListView.DataContext = null;
                 SinglesEtiquettesListView.DataContext = SinglesToEtiquettes;
             }
+        }
+
+        private void Generer_Etiquettes_Click(object sender, RoutedEventArgs e)
+        {
+            createEtiquettes(SinglesEtiquettesListView.Items);
+        }
+
+        private async void createEtiquettes(ItemCollection singlesForEtiquettes)
+        {
+            // Create empty PDF file.
+            bool exists = false;
+            StorageFile file = null;
+            try
+            {
+                file = await storageFolder.CreateFileAsync(path, CreationCollisionOption.FailIfExists);
+            }
+            catch (Exception alreadyExistsException)
+            {
+                exists = true;
+            }
+            if (exists == true)
+            {
+                file = await storageFolder.CreateFileAsync(path, CreationCollisionOption.OpenIfExists);
+                StorageFile sampleFileToDelete = await storageFolder.GetFileAsync(path);
+                await sampleFileToDelete.DeleteAsync();
+                file = await storageFolder.CreateFileAsync(path, CreationCollisionOption.OpenIfExists);
+            }
+
+            // Open to PDF file for read/write.
+            StorageFile sampleFile = await storageFolder.GetFileAsync(file.Name);
+            var stream = await sampleFile.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite);
+            
+            // Create an instance of the document class which represents the PDF document itself.
+            Document document = new Document(PageSize.A4, 25, 25, 30, 30);
+
+            // Create an instance to the PDF file by creating an instance of the PDF 
+            // Writer class using the document and the filestrem in the constructor.
+            PdfWriter writer = PdfWriter.GetInstance(document, stream.AsStream());
+
+            // Add meta information to the document
+            document.AddAuthor("Gilbert Rouchard");
+            document.AddCreator("Carlos' Software");
+            document.AddKeywords("PDF Etiquettes");
+            // document.AddSubject("");
+            document.AddTitle("Etiquettes pour jukebox.");
+
+            if (SinglesEtiquettesListView.Items.Count != 0)
+            {
+                // Open the document to enable you to write to the document
+                document.Open();
+                PdfPTable table = new PdfPTable(3);
+                int counter = 0;
+                foreach (SingleJoinDataViewModel item in SinglesEtiquettesListView.Items)
+                {
+                    table.AddCell(createEtiquette(item));
+                    counter = counter + 1;
+                }
+                int missingCells = counter % 3;
+                if (missingCells == 1)
+                {
+                    table.AddCell(createEmptyEtiquette());
+                    table.AddCell(createEmptyEtiquette());
+                }
+                else if (missingCells == 2)
+                {
+                    table.AddCell(createEmptyEtiquette());
+                }
+                document.Add(table);
+            }
+            // Close the document
+            document.Close();
+            // Close the writer instance
+            writer.Close();
+            await Launcher.LaunchFileAsync(sampleFile);
+            // await sampleFile.DeleteAsync();
+        }
+
+        private PdfPTable createEtiquette(SingleJoinDataViewModel item)
+        {
+            PdfPTable internalTable = new PdfPTable(1);
+            PdfPCell titreACell = new PdfPCell(new Phrase(item.TitreA));
+            PdfPCell titreBCell = new PdfPCell(new Phrase(item.TitreB));
+            PdfPCell artisteCell = new PdfPCell(new Phrase(item.Artiste));
+            titreACell.FixedHeight = 20;
+            artisteCell.FixedHeight = 40;
+            titreBCell.FixedHeight = 20;
+            internalTable.AddCell(titreACell);
+            internalTable.AddCell(artisteCell);
+            internalTable.AddCell(titreBCell);
+
+            return internalTable;
+        }
+
+        private PdfPTable createEmptyEtiquette()
+        {
+            PdfPTable internalTable = new PdfPTable(1);
+            PdfPCell titreACell = new PdfPCell();
+            PdfPCell titreBCell = new PdfPCell();
+            PdfPCell artisteCell = new PdfPCell();
+            titreACell.FixedHeight = 20;
+            artisteCell.FixedHeight = 40;
+            titreBCell.FixedHeight = 20;
+            internalTable.AddCell(titreACell);
+            internalTable.AddCell(artisteCell);
+            internalTable.AddCell(titreBCell);
+
+            return internalTable;
         }
     }
 }
