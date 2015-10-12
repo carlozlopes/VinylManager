ba1ef792b5198aa584b2fb89c9c6a5cc0aff8033
clopez <carloz.lopes@gmail.com>
2015-10-12 17:12:42 -0500

    Second version of EtiquettesPage

--- VinylManager/Views/EtiquettesPage.xaml.cs
+++ VinylManager/Views/EtiquettesPage.xaml.cs
@@ -2,6 +2,7 @@
 using iTextSharp.text.pdf;
 using iTextSharp.text;
 using iTextSharp;
+using System.Collections.ObjectModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -36,10 +37,106 @@ namespace VinylManager.Views
         string path = @"Etiquettes.pdf";
         // StorageFolder storageFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
         StorageFolder storageFolder = ApplicationData.Current.TemporaryFolder;
+        ObservableCollection<FontFamily> titresFonts = new ObservableCollection<FontFamily>();
+        ObservableCollection<Int32> titresFontSizes = new ObservableCollection<Int32>();
+        ObservableCollection<String> titresFontColors = new ObservableCollection<String>();
+        ObservableCollection<FontFamily> artisteFonts = new ObservableCollection<FontFamily>();
+        ObservableCollection<Int32> artisteFontSizes = new ObservableCollection<Int32>();
+        ObservableCollection<String> artisteFontColors = new ObservableCollection<String>();
+        String tFont;
+        Int32 tFontSize;
+        String tFontColor;
+        String aFont;
+        Int32 aFontSize;
+        String aFontColor;
+        
+        Font titreFont;
+        Font artisteFont;
+
 
         public EtiquettesPage()
         {
             this.InitializeComponent();
+            initializeFonts();
+        }
+
+        private void initializeFonts()
+        {
+            populateFonts(titresFonts);
+            populateFontSizes(titresFontSizes);
+            populateFontColors(titresFontColors);
+
+            populateFonts(artisteFonts);
+            populateFontSizes(artisteFontSizes);
+            populateFontColors(artisteFontColors);
+
+
+            TitresFontsCombo.DataContext = titresFonts;
+            TitresFontsCombo.SelectedIndex = 0;
+            TitresFontSizeCombo.DataContext = titresFontSizes;
+            TitresFontSizeCombo.SelectedIndex = 4;
+            TitresFontsColorCombo.DataContext = titresFontColors;
+            TitresFontsColorCombo.SelectedIndex = 1;
+
+            ArtisteFontsCombo.DataContext = artisteFonts;
+            ArtisteFontsCombo.SelectedIndex = 0;
+            ArtisteFontSizeCombo.DataContext = artisteFontSizes;
+            ArtisteFontSizeCombo.SelectedIndex = 6;
+            ArtisteFontsColorCombo.DataContext = artisteFontColors;
+            ArtisteFontsColorCombo.SelectedIndex = 10;
+        }
+
+        private void populateFonts(ObservableCollection<FontFamily> fonts)
+        {
+            fonts.Add(new FontFamily("Courier"));
+            fonts.Add(new FontFamily("Courier-Bold"));
+            fonts.Add(new FontFamily("Courier-BoldOblique"));
+            fonts.Add(new FontFamily("Courier-Oblique"));
+            fonts.Add(new FontFamily("Helvetica"));
+            fonts.Add(new FontFamily("Helvetica-Bold"));
+            fonts.Add(new FontFamily("Helvetica-BoldOblique"));
+            fonts.Add(new FontFamily("Helvetica-Oblique"));
+            fonts.Add(new FontFamily("Symbol"));
+            fonts.Add(new FontFamily("Times"));
+            fonts.Add(new FontFamily("Times-Bold"));
+            fonts.Add(new FontFamily("Times-BoldItalic"));
+            fonts.Add(new FontFamily("Times-Italic"));
+            fonts.Add(new FontFamily("Times-Roman"));
+            fonts.Add(new FontFamily("Times-ZapfDingbats"));
+        }
+
+        private void populateFontSizes(ObservableCollection<Int32> fontSizes)
+        {
+            fontSizes.Add(8);
+            fontSizes.Add(9);
+            fontSizes.Add(10);
+            fontSizes.Add(11);
+            fontSizes.Add(12);
+            fontSizes.Add(13);
+            fontSizes.Add(14);
+            fontSizes.Add(15);
+            fontSizes.Add(16);
+            fontSizes.Add(17);
+            fontSizes.Add(18);
+            fontSizes.Add(19);
+            fontSizes.Add(20);
+        }
+
+        private void populateFontColors(ObservableCollection<String> fontColors)
+        {
+            fontColors.Add("BLACK");
+            fontColors.Add("BLUE");
+            fontColors.Add("CYAN");
+            fontColors.Add("DARK_GRAY");
+            fontColors.Add("GRAY");
+            fontColors.Add("GREEN");
+            fontColors.Add("LIGHT_GRAY");
+            fontColors.Add("MAGENTA");
+            fontColors.Add("ORANGE");
+            fontColors.Add("PINK");
+            fontColors.Add("RED");
+            fontColors.Add("WHITE");
+            fontColors.Add("YELLOW");
         }
 
         private void Artiste_Search_Box_QuerySubmitted(SearchBox sender, SearchBoxQuerySubmittedEventArgs args)
@@ -179,6 +276,8 @@ namespace VinylManager.Views
             // document.AddSubject("");
             document.AddTitle("Etiquettes pour jukebox.");
 
+            setFonts();
+
             if (SinglesEtiquettesListView.Items.Count != 0)
             {
                 // Open the document to enable you to write to the document
@@ -213,9 +312,16 @@ namespace VinylManager.Views
         private PdfPTable createEtiquette(SingleJoinDataViewModel item)
         {
             PdfPTable internalTable = new PdfPTable(1);
-            PdfPCell titreACell = new PdfPCell(new Phrase(item.TitreA));
-            PdfPCell titreBCell = new PdfPCell(new Phrase(item.TitreB));
-            PdfPCell artisteCell = new PdfPCell(new Phrase(item.Artiste));
+            PdfPCell titreACell = new PdfPCell(new Phrase(item.TitreA, titreFont));
+            titreACell.HorizontalAlignment = PdfPCell.ALIGN_CENTER;
+            titreACell.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+            PdfPCell titreBCell = new PdfPCell(new Phrase(item.TitreB, titreFont));
+            titreBCell.HorizontalAlignment = PdfPCell.ALIGN_CENTER;
+            titreBCell.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+            PdfPCell artisteCell = new PdfPCell(new Phrase(item.Artiste, artisteFont));
+            artisteCell.HorizontalAlignment = PdfPCell.ALIGN_CENTER;
+            artisteCell.VerticalAlignment = PdfPCell.ALIGN_MIDDLE;
+
             titreACell.FixedHeight = 20;
             artisteCell.FixedHeight = 40;
             titreBCell.FixedHeight = 20;
@@ -241,5 +347,99 @@ namespace VinylManager.Views
 
             return internalTable;
         }
+
+        private void setFonts()
+        {
+            titreFont = FontFactory.GetFont(tFont, tFontSize, retrieveBaseColor(tFontColor));
+            artisteFont = FontFactory.GetFont(aFont, aFontSize, retrieveBaseColor(aFontColor));
+        }
+
+        private BaseColor retrieveBaseColor(String color)
+        {
+            BaseColor myColor;
+            switch (color)
+            {
+                case "BLACK":
+                    myColor = BaseColor.BLACK;
+                    break;
+                case "BLUE":
+                    myColor = BaseColor.BLUE;
+                    break;
+                case "CYAN":
+                    myColor = BaseColor.CYAN;
+                    break;
+                case "DARK_GRAY":
+                    myColor = BaseColor.DARK_GRAY;
+                    break;
+                case "GRAY":
+                    myColor = BaseColor.GRAY;
+                    break;
+                case "GREEN":
+                    myColor = BaseColor.GREEN;
+                    break;
+                case "LIGHT_GRAY":
+                    myColor = BaseColor.LIGHT_GRAY;
+                    break;
+                case "MAGENTA":
+                    myColor = BaseColor.MAGENTA;
+                    break;
+                case "ORANGE":
+                    myColor = BaseColor.ORANGE;
+                    break;
+                case "PINK":
+                    myColor = BaseColor.PINK;
+                    break;
+                case "RED":
+                    myColor = BaseColor.RED;
+                    break;
+                case "WHITE":
+                    myColor = BaseColor.WHITE;
+                    break;
+                case "YELLOW":
+                    myColor = BaseColor.YELLOW;
+                    break;
+                default:
+                    myColor = BaseColor.BLACK;
+                    break;
+            }
+            return myColor;
+        } 
+
+        private void FontsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tFont = ((FontFamily)TitresFontsCombo.SelectedItem).Source;
+        }
+
+        private void TitresFontSizeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tFontSize = ((Int32)TitresFontSizeCombo.SelectedItem);
+        }
+
+        private void TitresFontsColorCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tFontColor = ((String)TitresFontsColorCombo.SelectedItem);
+        }
+
+        private void ArtisteFontsCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            aFont = ((FontFamily)ArtisteFontsCombo.SelectedItem).Source;
+        }
+
+        private void ArtisteFontSizeCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            aFontSize = ((Int32)ArtisteFontSizeCombo.SelectedItem);
+        }
+
+        private void ArtisteFontsColorCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            aFontColor = ((String)ArtisteFontsColorCombo.SelectedItem);
+        }
+        
+
+        /* private void TitresColorCombo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tFontColor = ((BaseColor)Titres)
+            // titreFont.SetColor((BaseColor)TitresColorCombo.SelectedIndex);
+        } */
     }
 }
